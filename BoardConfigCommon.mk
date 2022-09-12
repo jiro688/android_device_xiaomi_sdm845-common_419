1f7542e026bc6108568e9450e9657d627d15f418
Gustavo Mendes <gusttavo.me@outlook.com>
2022-09-12 23:24:08 +0530

    sdm845-common: Set VENDOR_SECURITY_PATCH to platform

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -143,7 +143,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(COMMON_PATH)
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2020-12-01
+VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SELinux
 include device/lineage/sepolicy/libperfmgr/sepolicy.mk
